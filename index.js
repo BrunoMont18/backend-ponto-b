125b6448fc55be40a3e8cef9a71093d514c77914
Bruno Monteiro <bruno.mf18@sempreceub.com>
2024-10-29 10:46:27 -0300

    altercaçao

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("ola,mundo")
+let express = require ('express')
+let app = express ();
+
+app.get('/', (req, res) => {
+    res.send("Chamada ao recurso realizada com sucesso");
+});
+
+app.post('/rotapost')
+
+app.listen(3000, () => {
+
+    console.log("Servidor aguardando requisições")
+});
+
